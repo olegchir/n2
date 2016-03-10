94b176fb05bbd8ae9ca868c48a10c4f12fab5966
Oleg Chirukhin <olegchiruhin@gmail.com>
2016-03-10 21:02:15 +0600

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var express_1 = require('express');
+var index = express_1.Router();
+/* GET home page. */
+index.get('/', function (req, res, next) {
+    res.render('index', { title: 'Visual Studio Code!' });
+});
+/* GET Quick Start. */
+index.get('/quickstart', function (req, res, next) {
+    res.render('quickstart');
+});
+exports["default"] = index;
---
+++ src/routes/users.js
@@ -0,0 +1,8 @@
+"use strict";
+var express_1 = require('express');
+var users = express_1.Router();
+/* GET users listing. */
+users.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+exports["default"] = users;
